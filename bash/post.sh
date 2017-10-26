7ab73231a43d2ee8653af32d164d6d4a7ef4dd3e
andreaskaitis <andreaskaitis@gmail.com>
2017-10-26 03:21:29 +0300

    added command to change the initial port

--- bash/post.sh
+++ bash/post.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
+#sudo sysctl -w net.inet.ip.portrange.first=25000 #So there are enough ports for below request
 
 ab -p json.txt -T application/json -c 100  -n 40000 http://localhost:8080/events > test1.txt &
